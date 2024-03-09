3ce6270b34ce38b86a51788752e3cd1731296804
ALEZ-DEV <raidersfocus@gmail.com>
2024-03-09 23:49:15 +0100

    add some image

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,8 +1,9 @@
-use leptos::{component, IntoView, view};
+use leptos::{component, view, IntoView};
 
 #[component]
 pub fn HomePage() -> impl IntoView {
     view! {
         <h1>Welcom to celica.moe!</h1>
+        <img src="/celica_pointing.jpeg"/>
     }
 }
