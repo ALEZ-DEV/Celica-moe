bfc898a8a55191aa5766c5befb2991eddb435a42
ALEZ-DEV <raidersfocus@gmail.com>
2024-03-12 21:56:44 +0100

    the calendar start looking good

--- src/components/event.rs
+++ src/components/event.rs
@@ -18,17 +18,31 @@ pub fn EventComponent() -> impl IntoView {
 
     view! {
         <div class={DEFAULT_TAILWIND_CLASS}>
-            <div class="h-full w-[2px] bg-primary relative flex justify-center"></div>
+            <div class="bg-base-content w-[2px] bg-base-content absolute inset-x-2/3" style={move || {
+                            // set the height based on the number of item in the grid, one item height is 14 tailwind height * 0.25rem
+                            let style = "height: ";
+                            let height = match calendar() {
+                                Some(c) => format!("{}rem;", c.data.entries.len() as f32 * 14.0 * 0.25),
+                                None => "0rem;".to_string(),
+                            };
+                            format!("{} {}", style, height)
+                        }}></div>
             <div class="grid grid-cols-1 grid-rows-2">
                 {move || match calendar() {
                     Some(c) => c.data.entries.iter().map(|i| view! {
-                        <h3>{&i.name}</h3>
+                            <div class="h-14">
+                                <div class="h-12 my-1.5 rounded-lg bg-fixed bg-right bg-local" style="background-image: url('https://static.miraheze.org/pgrwiki/c/c3/Bannercweave.png')">
+                                    <div class="h-full rounded-lg bg-transparent px-2 text-left text-white font-bold bg-gradient-to-r from-black">
+                                        {&i.name}
+                                    </div>
+                                </div>
+                            </div>
                     }).collect_view(),
                     None => view! {
+
                         <span class="loading loading-spinner loading-lg m-auto"></span>
                     }.into_view()
                 }}
-                //<div class="h-24 bg-secondary"></div>
             </div>
         </div>
     }
